00edb18b998d9b610cb0a7141953660e51f6a6d6
Ahmed Ayman Mansour <ahmadahmad00790@yahoo.com>
2022-04-30 20:32:36 +0200

    Added some files

--- ProPreFlask/ProPreFlask/views.py
+++ ProPreFlask/ProPreFlask/views.py
@@ -2,8 +2,16 @@
 Routes and views for the flask application.
 """
 
+# Important modules for our ProPre
+import build_seq
+from io import StringIO
+from Bio import SeqIO
+from pymol import cmd
+
+
 from datetime import datetime
 from flask import render_template
+from flask import send_file
 from ProPreFlask import app
 
 @app.route('/')
@@ -35,3 +43,123 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+
+'''
+Old code for protein processing:
+'''
+
+def translate_rna(s):
+    '''
+    Returns the AminoAcids Sequence from RNA Codons.
+    '''
+    codon2aa = {"AAA":"K", "AAC":"N", "AAG":"K", "AAU":"N", 
+                "ACA":"T", "ACC":"T", "ACG":"T", "ACU":"T", 
+                "AGA":"R", "AGC":"S", "AGG":"R", "AGU":"S", 
+                "AUA":"I", "AUC":"I", "AUG":"M", "AUU":"I", 
+
+                "CAA":"Q", "CAC":"H", "CAG":"Q", "CAU":"H", 
+                "CCA":"P", "CCC":"P", "CCG":"P", "CCU":"P", 
+                "CGA":"R", "CGC":"R", "CGG":"R", "CGU":"R", 
+                "CUA":"L", "CUC":"L", "CUG":"L", "CUU":"L", 
+
+                "GAA":"E", "GAC":"D", "GAG":"E", "GAU":"D", 
+                "GCA":"A", "GCC":"A", "GCG":"A", "GCU":"A", 
+                "GGA":"G", "GGC":"G", "GGG":"G", "GGU":"G", 
+                "GUA":"V", "GUC":"V", "GUG":"V", "GUU":"V", 
+
+                "UAA":"_", "UAC":"Y", "UAG":"_", "UAU":"T", 
+                "UCA":"S", "UCC":"S", "UCG":"S", "UCU":"S", 
+                "UGA":"_", "UGC":"C", "UGG":"W", "UGU":"C", 
+                "UUA":"L", "UUC":"F", "UUG":"L", "UUU":"F"}
+
+    l = [codon2aa.get(s[n:n+3], 'X') for n in range(0, len(s), 3)]
+    return "".join(l)
+
+
+def translate(seq):
+    '''
+    Returns the AminoAcids Sequence from DNA Codons.
+    '''
+    table = {
+        'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+        'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+        'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',                
+        'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+        'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+        'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
+        'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
+        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
+        'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
+        'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
+        'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
+        'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W',
+    }
+    protein = ""
+    if len(seq)%3 == 0:
+        for i in range(0, len(seq), 3):
+            codon = seq[i:i + 3]
+            protein+= table[codon]
+    return protein
+
+def read_seq(inputfile):
+    '''
+    Returns the sequence of DNA Codons without line endings.
+    '''
+    with open(inputfile, "r") as f:
+        seq = f.read()
+    seq = seq.replace("\n", "")
+    seq = seq.replace("\r", "")
+    return seq
+
+def dna2rna(seq):
+    '''
+    Returns the RNA version of a DNA.
+    '''
+    temp = ''
+    temp = str(seq).replace('T', 'U')
+    return temp
+
+
+def readfasta():
+    '''
+    Returns all sequences from a fasta file.
+    '''
+    Reads = list()
+    with open('test.fasta', 'r') as lines:
+        for line in lines:
+            if not line.startswith('>'):
+                line = line.replace("\n", "")
+                line = line.replace("\r", "")
+                Reads.append(line[1:])
+    return Reads
+
+'''
+reads = readfasta()
+ 
+for read in reads:
+    rna = dna2rna(read)
+    print(translate_rna(rna))
+'''
+
+
+document = '_document'
+filename = 'P69905.fasta'
+#_type = 'polypro'
+_type = 'antiparallel'
+
+
+
+@app.route('/GetAmino/<string:seq>')
+def GetAmino(seq=''):
+    seq = '>Alpha\n' + seq + '\n'
+    for seq_record in SeqIO.parse(StringIO(seq), "fasta"):
+        build_seq.build_seq(seq_record.seq, _type)
+        cmd.cmd.select(document,"all")
+        cmd.cmd.save("temp.pdb", document, -1, 'pdb')
+        cmd.cmd.delete("all")
+    return send_file('temp.pdb')
